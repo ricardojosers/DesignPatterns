2ec390327d678396f31f2d5eddb789d0b0ab00d7
Ricardo Rodrigues <rjrsgithub@gmail.com>
2012-10-11 17:02:09 -0300

    Commiting Builder

---
+++ DesignPatterns/src/creational/builder/BoletoBuilder.java
@@ -0,0 +1,17 @@
+package creational.builder;
+
+import java.util.Calendar;
+
+public interface BoletoBuilder {
+	void buildSacado(String sacado);
+
+	void buildCedente(String cedente);
+
+	void buildValor(double valor);
+
+	void buildVencimento(Calendar vencimento);
+
+	void buildNossoNumero(int nossoNumero);
+
+	Boleto getBoleto();
+}
---
+++ DesignPatterns/src/creational/builder/TestaGeradorDeBoleto.java
@@ -0,0 +1,10 @@
+package creational.builder;
+
+public class TestaGeradorDeBoleto {
+	public static void main(String[] args) {
+		BoletoBuilder boletoBuilder = new BBBoletoBuilder();
+		GeradorDeBoleto geradorDeBoleto = new GeradorDeBoleto(boletoBuilder);
+		Boleto boleto = geradorDeBoleto.geraBoleto();
+		System.out.println(boleto);
+	}
+}
