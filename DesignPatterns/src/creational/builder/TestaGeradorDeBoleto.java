5d893a8415065f72c02f209e426f268af7c425cf
Ricardo Rodrigues <rjrsgithub@gmail.com>
2012-10-11 17:27:08 -0300

    Inclusão da mensagem

--- DesignPatterns/src/creational/builder/TestaGeradorDeBoleto.java
+++ DesignPatterns/src/creational/builder/TestaGeradorDeBoleto.java
@@ -1,7 +1,11 @@
 package creational.builder;
 
+import javax.swing.JOptionPane;
+
 public class TestaGeradorDeBoleto {
 	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Builder\nObjetivo: Separar o processo de construção de um objeto de sua representação e permitir a sua \n" +
+				"criação passo-a-passo.\n Diferentes tipos de objetos podem ser criados com implementações distintas de cada passo");
 		BoletoBuilder boletoBuilder = new BBBoletoBuilder();
 		GeradorDeBoleto geradorDeBoleto = new GeradorDeBoleto(boletoBuilder);
 		Boleto boleto = geradorDeBoleto.geraBoleto();
